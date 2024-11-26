7089a2887bdac27fa046b601d45b7f1d688eeca4
aoi <c0a2315791@edu.teu.ac.jp>
2024-11-26 17:05:28 +0900

    ex01

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,6 +1,7 @@
 import os
 import random
 import sys
+import time
 import pygame as pg
 
 
@@ -26,6 +27,29 @@ def check_bound(rct:pg.Rect) -> tuple[bool, bool]:
     if rct.top < 0 or HEIGHT < rct.bottom:
         tate = False
     return yoko, tate
+
+
+def gameover(screen: pg.Surface) -> None:
+    black_out = pg.Surface((WIDTH, HEIGHT))
+    pg.draw.rect(black_out, (0, 0, 0), pg.Rect(0, 0,WIDTH, HEIGHT))
+    black_out.set_alpha(50)
+    font_go = pg.font.Font(None, 80)
+    txt_go = font_go.render("Game Over", True, (255, 255, 255))
+    txt_go_rct = txt_go.get_rect()
+    txt_go_rct.center = WIDTH/2, HEIGHT/2
+    cry_ktn_img = pg.transform.rotozoom(pg.image.load("fig/8.png"), 0, 0.9)
+    cry_ktn_img_rct = cry_ktn_img.get_rect()
+    cry_ktn_img_rct.center = WIDTH/2-40*len("Game Over")/2, HEIGHT/2
+    screen.blit(black_out, [0, 0])
+    screen.blit(txt_go, txt_go_rct)
+    screen.blit(cry_ktn_img, cry_ktn_img_rct)
+    cry_ktn_img_rct.move_ip(40*len("Game Over"), 0)
+    screen.blit(cry_ktn_img, cry_ktn_img_rct)
+    pg.display.update()
+    time.sleep(5)
+    return
+    
+
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
@@ -65,8 +89,8 @@ def main():
         if check_bound(bb_rct) == (False, True):
             vx *= -1
         if bb_rct.colliderect(kk_rct):
+            gameover(screen)
             return
-
         screen.blit(kk_img, kk_rct)
         screen.blit(bb_img, bb_rct)
         pg.display.update()
